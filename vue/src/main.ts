17b6a05c0b9966bda51c924592bc2c15ca7a172c
nikita <nikita@etude.team>
2025-07-18 13:15:28 +0700

    Добавлен плагин pinia-plugin-persistedstate

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './style.css'
 import App from './App.vue'
 
-const pinia = createPinia();
+const pinia = createPinia().use(piniaPluginPersistedstate);
+
 createApp(App).use(pinia).mount('#app')
--- vue/src/stores/userStore.ts
+++ vue/src/stores/userStore.ts
@@ -2,54 +2,56 @@ import { defineStore } from 'pinia';
 import { ref, type Ref } from 'vue';
 import type {User} from "../types/user.ts";
 
-export const useUserStore = defineStore('user', () => {
-    const users: Ref<User[]> = ref([]);
+export const useUserStore = defineStore('user', {
+    state: () => {
+        const users: Ref<User[]> = ref([]);
+
+        /**
+         * Добавить пользователя
+         * @param formData
+         */
+        const addUser = (formData: User) => {
+            const user = formData;
+
+            // Валидация пароля для LDAP
+            if (user.type === 'LDAP' && !user.password) {
+                throw new Error('Для пользователя типа "LDAP" требует пароль');
+            }
+
+            users.value.push(user);
+        };
+
+        /**
+         * Обновить пользователя
+         * @param index
+         * @param formData
+         */
+        const updateUser = (index: number, formData: User) => {
+            const user = formData;
+
+            // Валидация пароля для LDAP
+            if (user.type === 'LDAP' && !user.password) {
+                throw new Error('Для пользователя типа "LDAP" требует пароль');
+            }
+
+            users.value[index] = user;
+        };
+
+        /**
+         * Удалить пользователя
+         * @param index
+         */
+        const deleteUser = (index: number) => {
+            users.value.splice(index, 1);
+        };
 
 
-    /**
-     * Добавить пользователя
-     * @param formData
-     */
-    const addUser = (formData: User) => {
-        const user = formData;
-
-        // Валидация пароля для LDAP
-        if (user.type === 'LDAP' && !user.password) {
-            throw new Error('Для пользователя типа "LDAP" требует пароль');
-        }
-
-        users.value.push(user);
-    };
-
-    /**
-     * Обновить пользователя
-     * @param index
-     * @param formData
-     */
-    const updateUser = (index: number, formData: User) => {
-        const user = formData;
-
-        // Валидация пароля для LDAP
-        if (user.type === 'LDAP' && !user.password) {
-            throw new Error('Для пользователя типа "LDAP" требует пароль');
-        }
-
-        users.value[index] = user;
-    };
-
-    /**
-     * Удалить пользователя
-     * @param index
-     */
-    const deleteUser = (index: number) => {
-        users.value.splice(index, 1);
-    };
-
-
-    return {
-        users,
-        addUser,
-        updateUser,
-        deleteUser,
-    };
+        return {
+            users,
+            addUser,
+            updateUser,
+            deleteUser,
+        };
+    },
+    persist: true,
 });
